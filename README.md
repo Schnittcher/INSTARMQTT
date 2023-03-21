e42d605178f79469076e0f8fc60e114246bef4f4
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-03-21 02:05:50 +0100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# INSTARMQTT
+
+Folgende Module beinhaltet das INSTARMQTT Repository:
+
+- __Netzwerk__ ([Dokumentation](Netzwerk))  
+	Kurze Beschreibung des Moduls.
+
+- __Multimedia__ ([Dokumentation](Multimedia))  
+	Kurze Beschreibung des Moduls.
+
+- __Features__ ([Dokumentation](Features))  
+	Kurze Beschreibung des Moduls.
+
+- __Alarm__ ([Dokumentation](Alarm))  
+	Kurze Beschreibung des Moduls.
+
+- __Tasks__ ([Dokumentation](Tasks))  
+	Kurze Beschreibung des Moduls.
+
+- __System__ ([Dokumentation](System))  
+	Kurze Beschreibung des Moduls.
+
+- __SmartHome__ ([Dokumentation](SmartHome))  
+	Kurze Beschreibung des Moduls.
---
+++ libs/InstarBaseModule.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/SymconModulHelper/VariableProfileHelper.php';
+
+class InstarBaseModule extends IPSModule
+{
+    use VariableProfileHelper;
+
+    public function Create()
+    {
+        parent::Create();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        $this->RegisterPropertyString('MQTTTopicPraefix', '');
+        $this->RegisterPropertyString('MQTTKlientID', '');
+
+        $Variables = [];
+        foreach (static::$Variables as $Pos => $Variable) {
+            $Variables[] = [
+                'Ident'        => str_replace(' ', '', $Variable[0]),
+                'Name'         => $this->Translate($Variable[1]),
+                'VarType'      => $Variable[2],
+                'Profile'      => $Variable[3],
+                'Action'       => $Variable[4],
+                'Pos'          => $Pos + 1,
+                'Keep'         => $Variable[5]
+            ];
+        }
+        $this->RegisterPropertyString('Variables', json_encode($Variables));
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        $NewRows = static::$Variables;
+        $NewPos = 0;
+        $Variables = json_decode($this->ReadPropertyString('Variables'), true);
+        foreach ($Variables as $Variable) {
+            $VariableActive = $Variable['Keep'];
+
+            @$this->MaintainVariable($Variable['Ident'], $Variable['Name'], $Variable['VarType'], $Variable['Profile'], $Variable['Pos'], $VariableActive);
+            if ($Variable['Action'] && $VariableActive) {
+                $this->EnableAction($Variable['Ident']);
+            }
+            foreach ($NewRows as $Index => $Row) {
+                if ($Variable['Ident'] == str_replace(' ', '', $Row[0])) {
+                    unset($NewRows[$Index]);
+                }
+            }
+            if ($NewPos < $Variable['Pos']) {
+                $NewPos = $Variable['Pos'];
+            }
+        }
+
+        if (count($NewRows) != 0) {
+            foreach ($NewRows as $NewVariable) {
+                $Variables[] = [
+                    'Ident'        => str_replace(' ', '', $NewVariable[0]),
+                    'Name'         => $this->Translate($NewVariable[1]),
+                    'VarType'      => $NewVariable[2],
+                    'Profile'      => $NewVariable[3],
+                    'Action'       => $NewVariable[4],
+                    'Pos'          => ++$NewPos,
+                    'Keep'         => $NewVariable[5]
+                ];
+            }
+            IPS_SetProperty($this->InstanceID, 'Variables', json_encode($Variables));
+            IPS_ApplyChanges($this->InstanceID);
+            return;
+        }
+    }
+
+    public function resetVariables()
+    {
+        $NewRows = static::$Variables;
+        $Variables = [];
+        foreach ($NewRows as $Pos => $Variable) {
+            $Variables[] = [
+                'Ident'        => str_replace(' ', '', $Variable[0]),
+                'Name'         => $this->Translate($Variable[1]),
+                'VarType'      => $Variable[2],
+                'Profile'      => $Variable[3],
+                'Action'       => $Variable[4],
+                'Pos'          => $Pos + 1,
+                'Keep'         => $Variable[5]
+            ];
+        }
+        IPS_SetProperty($this->InstanceID, 'Variables', json_encode($Variables));
+        IPS_ApplyChanges($this->InstanceID);
+        return;
+    }
+
+    protected function SetValue($Ident, $Value)
+    {
+        if (@$this->GetIDForIdent($Ident)) {
+            $this->SendDebug('SetValue :: ' . $Ident, $Value, 0);
+            parent::SetValue($Ident, $Value);
+        }
+    }
+
+    protected function sendMQTT($Topic, $Payload)
+    {
+        $result = true;
+
+        $Buffer['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Buffer['PacketType'] = 3;
+        $Buffer['QualityOfService'] = 0;
+        $Buffer['Retain'] = false;
+        $Buffer['Topic'] = $Topic;
+        $Buffer['Payload'] = $Payload;
+        $BufferJSON = json_encode($Buffer, JSON_UNESCAPED_SLASHES);
+        $BufferJSON = json_encode($Buffer);
+        $this->SendDebug(__FUNCTION__ . ' :: JSON', $BufferJSON, 0);
+        $result = @$this->SendDataToParent($BufferJSON);
+
+        if ($result === false) {
+            $last_error = error_get_last();
+            echo $last_error['message'];
+        }
+    }
+}
